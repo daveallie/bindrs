5e48cd9dfc2ea472c988a3345d65d9225e6ceb5a
Dave Allie <dave@tanda.co>
2017-01-18 17:20:47 +1000

    Fix - Can now compile

--- src/shared/startup.rs
+++ src/shared/startup.rs
@@ -1,6 +1,4 @@
 use regex::RegexSet;
-use std::thread;
-use std::sync::mpsc::channel;
 
 pub fn startup_common(base_dir: &str, ignores: &RegexSet) {
     let mut watcher = super::watcher::BindrsWatcher::new(base_dir, ignores);
--- src/shared/watcher.rs
+++ src/shared/watcher.rs
@@ -1,80 +1,78 @@
 use regex::RegexSet;
 use notify::{DebouncedEvent, RecommendedWatcher, Watcher, RecursiveMode, watcher};
-use std::sync::mpsc::{channel, Receiver, Sender};
 use std::time::Duration;
 use std::thread::{self, JoinHandle};
-use std::sync::mpsc::TryRecvError;
+use std::sync::mpsc::{channel, Receiver, Sender, TryRecvError};
 
 pub struct BindrsWatcher {
-    pub rx: Receiver<(u8, String)>,
-    final_tx: Sender<(u8, String)>,
-    watch_loop_rx: Receiver<u8>,
-    watch_loop_tx: Sender<u8>,
+    pub rx: Option<Receiver<(u8, String)>>,
     dir: String,
     ignores: RegexSet,
-    notify_rx: Receiver<DebouncedEvent>,
-    watcher: RecommendedWatcher,
+    watcher: Option<RecommendedWatcher>,
+    watch_loop_tx: Option<Sender<u8>>,
     thread: Option<JoinHandle<()>>
 }
 
 impl BindrsWatcher {
     pub fn new(base_dir: &str, ignores: &RegexSet) -> BindrsWatcher {
-        let (tx, notify_rx) = channel();
-        let (final_tx, rx) = channel();
-        let (watch_loop_tx, watch_loop_rx) = channel();
-
         BindrsWatcher {
-            rx: rx,
-            final_tx: final_tx,
-            watch_loop_tx: watch_loop_tx,
-            watch_loop_rx: watch_loop_rx,
+            rx: None,
+            watch_loop_tx: None,
             dir: base_dir.to_owned(),
             ignores: ignores.to_owned(),
-            notify_rx: notify_rx,
-            watcher: watcher(tx, Duration::from_millis(200)).unwrap(),
+            watcher: None,
             thread: None
         }
     }
 
     pub fn watch(&mut self) {
-        // Add a path to be watched. All files and directories at that path and
-        // below will be monitored for changes.
-        self.watcher.watch(&self.dir, RecursiveMode::Recursive).unwrap();
+        let (final_tx, final_rx) = channel();
+        let (notify_tx, notify_rx) = channel();
+        let (watch_loop_tx, watch_loop_rx) = channel();
 
-        self.thread = Some(thread::spawn(move || {
+        let mut watcher = watcher(notify_tx, Duration::from_millis(200)).unwrap();
+        watcher.watch(&self.dir, RecursiveMode::Recursive).unwrap();
+        self.watcher = Some(watcher);
+        self.watch_loop_tx = Some(watch_loop_tx);
+        self.rx = Some(final_rx);
+
+        self.thread = {
             let dir_length = self.dir.len();
-            loop {
-                let event = self.notify_rx.recv().unwrap_or_else(|e| panic!("watch error: {:?}", e));
-                match self.watch_loop_rx.try_recv() {
-                    Ok(_) | Err(TryRecvError::Disconnected) => break,
-                    Err(TryRecvError::Empty) => ()
-                };
-                let actions = match event {
-                    DebouncedEvent::Create(p) |
-                    DebouncedEvent::Write(p) => vec![(0, p)],
-                    DebouncedEvent::Remove(p) => vec![(1, p)],
-                    DebouncedEvent::Rename(p1, p2) => vec![(1, p1), (0, p2)],
-                    _ => vec![]
-                };
+            let ignores = self.ignores.clone();
+            Some(thread::spawn(move || {
+                loop {
+                    let event = notify_rx.recv().unwrap_or_else(|e| panic!("watch error: {:?}", e));
+                    match watch_loop_rx.try_recv() {
+                        Ok(_) | Err(TryRecvError::Disconnected) => break,
+                        Err(TryRecvError::Empty) => ()
+                    };
+                    let actions = match event {
+                        DebouncedEvent::Create(p) |
+                        DebouncedEvent::Write(p) => vec![(0, p)],
+                        DebouncedEvent::Remove(p) => vec![(1, p)],
+                        DebouncedEvent::Rename(p1, p2) => vec![(1, p1), (0, p2)],
+                        _ => vec![]
+                    };
 
-                let filtered_actions = actions.into_iter().map(|(ref t, ref p)| {
-                    let short_path: String = p.to_str().unwrap().chars().skip(dir_length).collect();
-                    (*t as u8, short_path)
-                }).filter(|&(_, ref short_path)| !self.ignores.is_match(&short_path));
+                    let filtered_actions = actions.into_iter().map(|(ref t, ref p)| {
+                        let short_path: String = p.to_str().unwrap().chars().skip(dir_length).collect();
+                        (*t as u8, short_path)
+                    }).filter(|&(_, ref short_path)| !ignores.is_match(&short_path));
 
-                for (ref t, ref p) in filtered_actions {
-                    let _ = self.final_tx.send((*t, p.to_owned()));
+                    for (ref t, ref p) in filtered_actions {
+                        let _ = final_tx.send((*t, p.to_owned()));
+                    }
                 }
-            }
-        }));
+            }))
+        };
     }
 
     pub fn unwatch(&mut self) {
-        if  self.thread.is_some() {
-            self.watch_loop_tx.send(1);
+        if self.watcher.is_some() && self.thread.is_some() && self.watch_loop_tx.is_some() {
+            let _ = self.watch_loop_tx.clone().unwrap().send(1);
             self.thread = None;
+            self.watcher.take().unwrap().unwatch(&self.dir).unwrap();
+            self.watcher = None;
         }
-
-        self.watcher.unwatch(&self.dir).unwrap();
     }
 }
