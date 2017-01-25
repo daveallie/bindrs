b001cad17b43ade76595ed72f0e4b0720eba239c
Dave Allie <dave@daveallie.com>
2017-01-26 07:44:17 +1000

    Add package step to build and strip

--- development/build_and_strip.sh
+++ development/build_and_strip.sh
@@ -1,10 +1,27 @@
 #!/bin/bash
 
+set -e
+
 cd "$(dirname $0)"
 cd ..
 
-rm target/release/bindrs &>/dev/null
+rm ./target/release/bindrs &>/dev/null || true
+FULL_TOOLCHAIN="$(rustup toolchain list | grep default | awk '{print $1}' | cut -d '-' -f2-)"
 cargo build --release
-echo "Before Strip: $(ls -lh target/release/bindrs | awk '{print $5}')"
-strip target/release/bindrs
-echo " After Strip: $(ls -lh target/release/bindrs | awk '{print $5}')"
+
+echo "Before Strip: $(ls -lh ./target/release/bindrs | awk '{print $5}')"
+strip ./target/release/bindrs
+echo " After Strip: $(ls -lh ./target/release/bindrs | awk '{print $5}')"
+
+VERSION="$(./target/release/bindrs -V | awk '{print $2}')"
+FILENAME="bindrs-$VERSION-$FULL_TOOLCHAIN.tar.gz"
+
+cd ./target/release
+tar -zcf $FILENAME ./bindrs
+echo "GZipped File: $(ls -lh $FILENAME | awk '{print $5}')"
+
+cd ../..
+mkdir -p ./pkg
+mv ./target/release/$FILENAME ./pkg/
+
+echo "Built and zipped to ./pkg/$FILENAME"
