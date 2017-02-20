db35bc868cbccee199c8b54663806357bc750851
Dave Allie <dave@daveallie.com>
2017-02-21 07:05:58 +1000

    Add tests to helpers module

--- src/helpers.rs
+++ src/helpers.rs
@@ -1,6 +1,10 @@
 use regex::RegexSet;
+#[cfg(test)]
+use slog::Discard;
 use slog::Logger;
 use std::{thread, time};
+#[cfg(test)]
+use std::env::current_dir;
 use std::fs::canonicalize;
 use std::path::Path;
 use std::process::exit;
@@ -34,6 +38,16 @@ pub fn process_ignores(log: &Logger, vec: &mut Vec<String>) -> RegexSet {
 }
 
 fn vec_to_regex_set(log: &Logger, ignores: &[String]) -> RegexSet {
+    match RegexSet::new(&convert_to_project_regex_strings(ignores)[..]) {
+        Ok(rs) => rs,
+        Err(e) => {
+            log_error_and_exit(log, &format!("Provided regex failed to parse: {}", e));
+            panic!() // For compilation
+        }
+    }
+}
+
+fn convert_to_project_regex_strings<'a>(ignores: &'a [String]) -> Vec<String> {
     let mut regexes: Vec<String> = vec![];
 
     for i in ignores.iter() {
@@ -44,11 +58,64 @@ fn vec_to_regex_set(log: &Logger, ignores: &[String]) -> RegexSet {
         regexes.push(ignore)
     }
 
-    match RegexSet::new(&regexes[..]) {
-        Ok(rs) => rs,
-        Err(e) => {
-            log_error_and_exit(log, &format!("Provided regex failed to parse: {}", e));
-            panic!() // For compilation
-        }
+    regexes
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn test_logger() -> Logger {
+        Logger::root(Discard, o!())
+    }
+
+    #[test]
+    fn including_custom_ignore_skips_git() {
+        let mut strings: Vec<String> = vec![];
+        strings.push("^something$".to_owned());
+        let regex_set = process_ignores(&test_logger(), &mut strings);
+        assert!(regex_set.is_match("something"));
+        assert!(regex_set.is_match(".bindrsasdf"));
+        assert!(!regex_set.is_match(".git/something"));
+    }
+
+    #[test]
+    fn excluding_custom_ignore_includes_git() {
+        let mut strings: Vec<String> = vec![];
+        let regex_set = process_ignores(&test_logger(), &mut strings);
+        assert!(!regex_set.is_match("something"));
+        assert!(regex_set.is_match(".bindrsasdf"));
+        assert!(regex_set.is_match(".git/something"));
+    }
+
+    #[test]
+    fn regex_strings_are_not_modified() {
+        let mut strings: Vec<String> = vec![];
+        strings.push("^something$".to_owned());
+        let regex_set = process_ignores(&test_logger(), &mut strings);
+        assert!(regex_set.is_match("something"));
+        assert!(!regex_set.is_match("somethin"));
+        assert!(!regex_set.is_match("somethingg"));
+        assert!(!regex_set.is_match("something/somethingelse"));
+    }
+
+    #[test]
+    fn non_regex_strings_are_modified() {
+        let mut strings: Vec<String> = vec![];
+        strings.push("something".to_owned());
+        let regex_set = process_ignores(&test_logger(), &mut strings);
+        assert!(regex_set.is_match("something"));
+        assert!(!regex_set.is_match("somethin"));
+        assert!(!regex_set.is_match("somethingg"));
+        assert!(regex_set.is_match("something/somethingelse"));
+    }
+
+    #[test]
+    fn resolve_path_canonicalize_correctly() {
+        let mut path = current_dir().unwrap();
+        path.push("src");
+        assert_eq!(canonicalize(path.as_path()).unwrap().to_str().unwrap(),
+                   resolve_path("./src/processors/../../src").unwrap());
     }
 }
