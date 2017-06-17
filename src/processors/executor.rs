995b5e28d35616102b8e494b5330ab3fbee0a40c
Dave Allie <dave@daveallie.com>
2017-06-18 00:56:06 +1000

    Add log lines per second if sendning/recieving files

--- src/processors/executor.rs
+++ src/processors/executor.rs
@@ -5,7 +5,9 @@ use std::io::{Read, Write, BufWriter, BufReader};
 use std::marker::Send;
 use std::path::Path;
 use std::sync::{Arc, Mutex};
-use std::thread;
+use std::sync::mpsc::{self, TryRecvError, Receiver};
+use std::thread::{self, sleep};
+use std::time::Duration;
 use structs::bound_file::{BoundFile, FileAction};
 use structs::watcher::BindrsWatcher;
 use time;
@@ -20,22 +22,41 @@ pub fn start<R: Read + Send + 'static, W: Write + Send + 'static>(
     let lock: Arc<Mutex<Vec<(String, i64, i32)>>> = Arc::new(Mutex::new(vec![]));
     let lock_clone = lock.clone();
 
+    let sync_count: Arc<Mutex<(u32, u32)>> = Arc::new(Mutex::new((0, 0)));
+
     let base_dir_clone = base_dir.to_owned();
     let log_clone = log.clone();
+    let sync_count_clone = sync_count.clone();
     let child_1 = thread::spawn(move || {
-        run_local_watcher(&log_clone, &base_dir_clone, &ignores, writer, lock_clone);
+        run_local_watcher(
+            &log_clone,
+            &base_dir_clone,
+            &ignores,
+            writer,
+            lock_clone,
+            sync_count_clone,
+        );
     });
 
     let base_dir_clone = base_dir.to_owned();
     let log_clone = log.clone();
+    let sync_count_clone = sync_count.clone();
     let child_2 = thread::spawn(move || {
-        run_remote_listener(&log_clone, &base_dir_clone, reader, lock);
+        run_remote_listener(&log_clone, &base_dir_clone, reader, lock, sync_count_clone);
+    });
+
+    let log_clone = log.clone();
+    let (status_log_tx, status_log_rx) = mpsc::channel();
+    let child_3 = thread::spawn(move || {
+        run_status_logger(&log_clone, sync_count, &status_log_rx);
     });
 
     info!(log, "Ready!");
 
     let _ = child_1.join();
     let _ = child_2.join();
+    status_log_tx.send(()).unwrap_or_default();
+    let _ = child_3.join();
     info!(log, "BindRS Stopping");
 }
 
@@ -45,6 +66,7 @@ fn run_local_watcher<W: Write>(
     ingores: &RegexSet,
     writer: W,
     lock: Arc<Mutex<Vec<(String, i64, i32)>>>,
+    sync_count: Arc<Mutex<(u32, u32)>>,
 ) {
     let mut writer = BufWriter::new(writer);
     let mut watcher = BindrsWatcher::new(base_dir, ingores);
@@ -110,6 +132,17 @@ fn run_local_watcher<W: Write>(
             let bf = BoundFile::build_from_path_action(base_dir, p, a);
             debug!(log, "Sending {} to remote", bf.path);
             bf.to_writer(&mut writer);
+
+            {
+                let mut synced_nums = sync_count.lock().unwrap_or_else(|_| {
+                    helpers::log_error_and_exit(
+                        log,
+                        "Failed to aquire sync count lock, lock poisoned",
+                    );
+                    panic!()
+                });
+                synced_nums.0 += 1;
+            }
         }
     }
 }
@@ -119,6 +152,7 @@ fn run_remote_listener<R: Read>(
     base_dir: &str,
     reader: R,
     lock: Arc<Mutex<Vec<(String, i64, i32)>>>,
+    sync_count: Arc<Mutex<(u32, u32)>>,
 ) {
     let mut reader = BufReader::new(reader);
     loop {
@@ -135,5 +169,47 @@ fn run_remote_listener<R: Read>(
             (now_spec.sec, now_spec.nsec)
         };
         recent_files.push((bf.path, now_s, now_nano_s));
+
+        {
+            let mut synced_nums = sync_count.lock().unwrap_or_else(|_| {
+                helpers::log_error_and_exit(log, "Failed to aquire sync count lock, lock poisoned");
+                panic!()
+            });
+            synced_nums.1 += 1;
+        }
+    }
+}
+
+fn run_status_logger(log: &Logger, sync_count: Arc<Mutex<(u32, u32)>>, rx: &Receiver<()>) {
+    loop {
+        sleep(Duration::from_millis(1000));
+        match rx.try_recv() {
+            Ok(_) |
+            Err(TryRecvError::Disconnected) => break,
+            Err(TryRecvError::Empty) => (),
+        }
+
+        {
+            let mut synced_nums = sync_count.lock().unwrap_or_else(|_| {
+                helpers::log_error_and_exit(log, "Failed to aquire sync count lock, lock poisoned");
+                panic!()
+            });
+
+            let to_log = synced_nums.0 > 0 || synced_nums.1 > 0;
+            let mut message: Vec<String> = vec![];
+
+            if synced_nums.0 > 0 {
+                message.push(format!("Sent {} file/s.", synced_nums.0));
+            }
+
+            if synced_nums.1 > 0 {
+                message.push(format!("Received {} file/s.", synced_nums.1));
+            }
+
+            if to_log {
+                info!(log, "{}", message.join(" "));
+                *synced_nums = (0, 0)
+            }
+        }
     }
 }
